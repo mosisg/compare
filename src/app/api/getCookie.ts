582af9f05def66edfd3da2b5539d4172e7a964c6
Moussa <mosisg7@gmail.com>
2024-06-02 10:58:31 +0100

    Mise à jour des fichiers et ajout de nouvelles images et composants

--- src/app/api/getCookie.ts
+++
@@ -1,7 +0,0 @@
-import { parse } from 'cookie';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const cookies = parse(req.headers.cookie || '');
-  res.status(200).json({ cookies });
-}
